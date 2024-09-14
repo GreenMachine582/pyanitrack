ac93227d805d0d08d99afb6cd559f10d1cfd96da
Matthew <greenchicken1902@gmail.com>
2024-09-14 12:10:58 +1000

    refactor(database schema): updated v2 create db schema

--- src/pyanitrack/database/v2_create_schema.sql
+++ src/pyanitrack/database/v2_create_schema.sql
@@ -10,14 +10,6 @@ DO $$
 BEGIN
     IF NOT EXISTS (SELECT 1 FROM schema_version WHERE version = 2) THEN
 
-        -- Users Table
-        CREATE TABLE users (
-            id SERIAL PRIMARY KEY,
-            username VARCHAR(50) UNIQUE NOT NULL,
-            email VARCHAR(100) UNIQUE NOT NULL,
-            password_hash VARCHAR(255) NOT NULL
-        );
-
         -- Genres Table
         CREATE TABLE genre (
             id SERIAL PRIMARY KEY,
@@ -34,22 +26,22 @@ BEGIN
         -- Anime Table
         CREATE TABLE anime (
             id SERIAL PRIMARY KEY,
-            name VARCHAR(100) UNIQUE NOT NULL,
-            display_name VARCHAR(100) NOT NULL,
-            summary TEXT,
-            url VARCHAR(255),
-            thumbnail_url VARCHAR(255)
+            name VARCHAR(150) UNIQUE NOT NULL,
+            display_name VARCHAR(150) NOT NULL
         );
 
         -- Seasons Table
         CREATE TABLE season (
             id SERIAL PRIMARY KEY,
-            uid VARCHAR(50) UNIQUE NOT NULL,
             number INTEGER NOT NULL,
             anime INTEGER REFERENCES anime(id) ON DELETE CASCADE,
             episode_count INTEGER NOT NULL,
             myanimelist_url VARCHAR(255),
-            summary TEXT
+            mal_id INTEGER NOT NULL,
+            thumbnail_url VARCHAR(255),
+            ep_duration INTEGER NOT NULL,
+            summary TEXT,
+            UNIQUE (anime, number)
         );
 
         -- Episodes Table
@@ -58,53 +50,17 @@ BEGIN
             season INTEGER REFERENCES season(id) ON DELETE CASCADE,
             number INTEGER NOT NULL,
             anime INTEGER REFERENCES anime(id) ON DELETE CASCADE,
-            title VARCHAR(255) NOT NULL
+            UNIQUE (season, number)
         );
 
         -- Watch History Table
         CREATE TABLE watch_history (
             id SERIAL PRIMARY KEY,
-            user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
             anime INTEGER REFERENCES anime(id) ON DELETE CASCADE,
             season INTEGER REFERENCES season(id),
             date TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
-            progress INTEGER NOT NULL,
-            completion_percentage FLOAT NOT NULL,
-            score INTEGER CHECK (score >= 1 AND score <= 10)
-        );
-
-        -- Anime Status Table
-        CREATE TABLE anime_status (
-            id SERIAL PRIMARY KEY,
-            name VARCHAR(50) UNIQUE NOT NULL
-        );
-
-        -- User Anime Status Table
-        CREATE TABLE user_anime_status (
-            id SERIAL PRIMARY KEY,
-            user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-            anime INTEGER REFERENCES anime(id) ON DELETE CASCADE,
-            status INTEGER REFERENCES anime_status(id),
-            last_updated TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL
-        );
-
-        -- Anime Reviews Table
-        CREATE TABLE review_anime (
-            id SERIAL PRIMARY KEY,
-            user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-            anime INTEGER REFERENCES anime(id) ON DELETE CASCADE,
-            review_text TEXT NOT NULL,
-            date TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL
-        );
-
-        -- Episode Reviews Table
-        CREATE TABLE review_episode (
-            id SERIAL PRIMARY KEY,
-            user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-            episode INTEGER REFERENCES episode(id) ON DELETE CASCADE,
-            review_text TEXT NOT NULL,
-            date TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
-            score INTEGER CHECK (score >= 1 AND score <= 10)
+            eps_watched INTEGER NOT NULL,
+            completion_percentage FLOAT NOT NULL
         );
 
         -- Anime-Genre Join Table
