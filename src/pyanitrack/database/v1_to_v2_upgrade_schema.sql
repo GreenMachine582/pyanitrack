fb073e61b5908b4ca9625d350c873415914e1b34
Matthew <greenchicken1902@gmail.com>
2024-09-14 12:08:51 +1000

    refactor(database schema): updated v1 to v2 upgrade db schema

--- src/pyanitrack/database/v1_to_v2_upgrade_schema.sql
+++ src/pyanitrack/database/v1_to_v2_upgrade_schema.sql
@@ -13,19 +13,75 @@ BEGIN
         -- Rename old anime table for backup
         ALTER TABLE anime RENAME TO anime_old;
 
+        -- Genres Table
+        CREATE TABLE genre (
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(50) UNIQUE NOT NULL
+        );
+
+        -- Stream Services Table
+        CREATE TABLE stream_service (
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(50) UNIQUE NOT NULL,
+            domain_url VARCHAR(255)
+        );
+
         -- Create new anime table with updated structure
         CREATE TABLE anime (
             id SERIAL PRIMARY KEY,
-            name VARCHAR(100) UNIQUE NOT NULL,
-            display_name VARCHAR(100) NOT NULL,
+            name VARCHAR(150) UNIQUE NOT NULL,
+            display_name VARCHAR(150) NOT NULL
+        );
+
+        -- Seasons Table
+        CREATE TABLE season (
+            id SERIAL PRIMARY KEY,
+            number INTEGER NOT NULL,
+            anime INTEGER REFERENCES anime(id) ON DELETE CASCADE,
+            episode_count INTEGER NOT NULL,
+            myanimelist_url VARCHAR(255),
+            mal_id INTEGER NOT NULL,
+            thumbnail_url VARCHAR(255),
+            ep_duration INTEGER NOT NULL,
             summary TEXT,
-            url VARCHAR(255),
-            service VARCHAR(50),
-            thumbnail_url VARCHAR(255)
+            UNIQUE (anime, number)
+        );
+
+        -- Episodes Table
+        CREATE TABLE episode (
+            id SERIAL PRIMARY KEY,
+            season INTEGER REFERENCES season(id) ON DELETE CASCADE,
+            number INTEGER NOT NULL,
+            anime INTEGER REFERENCES anime(id) ON DELETE CASCADE,
+            UNIQUE (season, number)
+        );
+
+        -- Watch History Table
+        CREATE TABLE watch_history (
+            id SERIAL PRIMARY KEY,
+            anime INTEGER REFERENCES anime(id) ON DELETE CASCADE,
+            season INTEGER REFERENCES season(id),
+            date TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
+            eps_watched INTEGER NOT NULL,
+            completion_percentage FLOAT NOT NULL
+        );
+
+        -- Anime-Genre Join Table
+        CREATE TABLE anime_genre (
+            anime_id INTEGER REFERENCES anime(id) ON DELETE CASCADE,
+            genre_id INTEGER REFERENCES genre(id) ON DELETE CASCADE,
+            PRIMARY KEY (anime_id, genre_id)
+        );
+
+        -- Anime-Stream Service Join Table
+        CREATE TABLE anime_stream_service (
+            anime_id INTEGER REFERENCES anime(id) ON DELETE CASCADE,
+            stream_service_id INTEGER REFERENCES stream_service(id) ON DELETE CASCADE,
+            PRIMARY KEY (anime_id, stream_service_id)
         );
 
         -- Insert Initial Version Record
-        INSERT INTO schema_version (version, description) VALUES (2, 'Updated anime table structure');
+        INSERT INTO schema_version (version, description) VALUES (2, 'v1-v2: Split the anime table in relational tables');
 
     END IF;
 END $$;
