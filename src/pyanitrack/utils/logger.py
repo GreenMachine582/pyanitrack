30d832e6a34ee8e25ca3eff003381aff8a24ab75
Matthew Johnson <49015217+GreenMachine582@users.noreply.github.com>
2024-10-12 12:36:03 +1100

    refactor(logger): removed env and config dependency from logger for ease of testing and the purpose of single use

--- src/pyanitrack/utils/logger.py
+++ src/pyanitrack/utils/logger.py
@@ -4,6 +4,7 @@ import time
 from datetime import datetime
 from logging.handlers import RotatingFileHandler
 from os import listdir, makedirs, path, remove as os_remove
+from typing import Any
 
 _logger = logging.getLogger(__name__)
 logging.getLogger("requests").setLevel(logging.WARNING)
@@ -26,60 +27,48 @@ def _buildLogDirectory(logs_dir: str = "", add_file_handler: bool = True):
 
 
 class LoggerHandler:
+    DEFAULT_CONFIG: dict[str: Any] = {
+        "ext": "log",
+        "log_level": "INFO",
+        "add_console_handler": True,
+        "add_file_handler": False,
+        "add_instance": True,
+        "add_time_stamp": True,
+        "max_bytes": 10 * 1024 * 1024,  # 10MB
+        "backup_count": 5,
+        "age_limit": 60 * 60 * 24 * 7  # 7 days
+    }
 
-    EXT: str = "log"
-    LOG_LEVEL: str = "info"
-    ADD_CONSOLE_HANDLER: bool = True
-    ADD_FILE_HANDLER: bool = False
-
-    # Log file name generation
-    ADD_INSTANCE: bool = True
-    ADD_TIME_STAMP: bool = True
-
-    # File loggers with rotating handler
-    MAX_BYTES = 10 * 1024 * 1024  # 10MB
-    BACKUP_COUNT = 5
-    AGE_LIMIT = 60 * 60 * 24 * 7  # 7 days
-
-    def __init__(self, env, logs_dir: str = ""):
-        self.env = env
+    def __init__(self, logs_dir: str = "", **kwargs):
         self.logs_dir = logs_dir
+
+        self.config: dict[str: Any] = {**self.DEFAULT_CONFIG, **kwargs}
+
+        # Validate config
+        ext = self.config["ext"]
+        self.config["ext"] = ext.strip(".")
+        _validateLogLevel(self.config["log_level"])
+
+        # Build log directory and logger
         self.timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
         _buildLogDirectory(self.logs_dir, self.config["add_file_handler"])
 
-        self.log_level = self._getConfig("log_level").upper()
         # Clean old logs if applicable
         if self.logs_dir:
             self.cleanLogs()
 
-        # Validate log level
-        _validateLogLevel(self.log_level)
-
         self.logger = self._buildLogger()
-
-    @property
-    def config(self):
-        """Return references config from environment."""
-        return self.env.config
-
-    def _getConfig(self, key: str, section: str = "logs"):
-        """Get value from provided config or use class defined config."""
-        logs_config = self.env.config.get(section or "")
-        if key in logs_config:  # Configured value
-            return logs_config[key]
-        elif hasattr(self, key.upper()):  # Return class default
-            return getattr(self, key.upper())
 
     def _getLogFileName(self, level: str) -> str:
         """Constructs the filename of the log file."""
-        names = [self._getConfig("project_name", "project")]
-        if self.ADD_INSTANCE and self.env.instance:
-            names.append(self.env.instance)
+        names = [self.config.get("project_name", "project")]
+        if self.config["add_instance"] and self.config.get("instance"):
+            names.append(self.config.get("instance"))
         names.append(level)
-        if self._getConfig('add_time_stamp'):
+        if self.config["add_time_stamp"]:
             names.append(self.timestamp)
         names = filter(lambda x: x, names)
-        return f"{'_'.join(names)}.{self._getConfig('ext')}"
+        return f"{'_'.join(names)}.{self.config['ext']}"
 
     def _buildLogger(self):
         """Build the logger by creating and attaching handlers."""
@@ -90,8 +79,8 @@ class LoggerHandler:
         logger.setLevel(logging.DEBUG)
 
         # File loggers
-        if self.logs_dir and self._getConfig('add_file_handler'):
-            max_bytes, backup_count = self._getConfig('max_bytes'), self._getConfig('backup_count')
+        if self.logs_dir and self.config["add_file_handler"]:
+            max_bytes, backup_count = self.config["max_bytes"], self.config["backup_count"]
             for level in ['debug', 'info', 'warning']:
                 file_path = path.abspath(f"{self.logs_dir}/" + self._getLogFileName(level))
                 file_handler = RotatingFileHandler(file_path, maxBytes=max_bytes, backupCount=backup_count)
@@ -100,9 +89,9 @@ class LoggerHandler:
                 logger.addHandler(file_handler)
 
         # Console logger
-        if self._getConfig('add_console_handler'):
+        if self.config["add_console_handler"]:
             console_handler = logging.StreamHandler()
-            console_handler.setLevel(getattr(logging, self.log_level.upper()))
+            console_handler.setLevel(getattr(logging, self.config["log_level"].upper()))
             console_handler.setFormatter(log_formatter)
             logger.addHandler(console_handler)
 
@@ -111,11 +100,11 @@ class LoggerHandler:
     def cleanLogs(self):
         """Remove old log files (clean cache)"""
         now = time.time()
-        age_limit = self._getConfig("age_limit")
+        age_limit = self.config["age_limit"]
 
         for filename in listdir(self.logs_dir):
             file_path = path.abspath(f"{self.logs_dir}/" + filename)
-            if not path.isfile(file_path) or not filename.endswith(self._getConfig('ext')):
+            if not path.isfile(file_path) or not filename.endswith(self.config["ext"]):
                 continue  # Not expected log file or ext
             creation_time = path.getctime(file_path)
             if now - creation_time < age_limit:
